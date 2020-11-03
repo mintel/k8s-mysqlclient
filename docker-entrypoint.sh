3368d8a38732849bbfe93ea11df14edd8d127180
Nick <nickbadger1980@googlemail.com>
2020-11-03 10:44:08 +0000

    Remove debug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,6 @@ KUBERNETES_SERVICE_HOST=${KUBERNETES_SERVICE_HOST:-}
 # Grab db-values from vault if vault is enabled and we are running
 # inside kubernetes.
 if [ -n "${KUBERNETES_SERVICE_HOST}" ] && [ "${VAULT_ENABLED}" = "1" ]; then
-  echo "in"
   DB_NAME=$(vault-env env | grep DB_NAME | cut -d"=" -f2)
   DB_USER=$(vault-env env | grep DB_USER | cut -d"=" -f2)
   DB_PASSWORD=$(vault-env env | grep DB_PASSWORD | cut -d"=" -f2)
